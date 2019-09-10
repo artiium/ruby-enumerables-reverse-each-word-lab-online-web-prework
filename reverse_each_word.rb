31619ba4a20a6435d920f42dac4e5b7a4c5c749f
Artiium Arbyummi <artiium21@gmail.com>
2019-09-10 00:28:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,7 @@
-def reverse_each_word
-  
+original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
 end
